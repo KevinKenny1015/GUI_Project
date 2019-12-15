e32665fe704837600beee8633489c6780d8665e3
Kevin <kevinknn1@gmail.com>
2019-12-14 21:40:16 -0500

    Sprint 3 commit

---
+++ src/sample/Widget.java
@@ -0,0 +1,11 @@
+package sample;
+
+/**
+ * Widget extends Product.
+ */
+class Widget extends Product {
+
+  Widget(String name, String manufacturer, ItemType type) {
+    super(name, manufacturer, type);
+  }
+}
